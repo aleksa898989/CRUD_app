72c744d7e172f321469390b1e734bf04ed6a8ede
aleksa <aleksa.zdravkovic1996@gmail.com>
2021-04-10 15:51:15 +0200

    added validation and notification containing response from server

--- src/components/posts/add/add.jsx
+++ src/components/posts/add/add.jsx
@@ -1,9 +1,20 @@
 import { MDBContainer, MDBIcon } from "mdbreact";
 import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import Notification from "layout/notification";
 
 const AddPost = () => {
-  const { register, handleSubmit } = useForm();
+  const [response, setResponse] = useState("");
+  const [message, setMessage] = useState("");
+  const [showNotification, setShowNotification] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    mode: "onBlur",
+  });
   const onSubmit = async (data) => {
     try {
       fetch("https://jsonplaceholder.typicode.com/posts", {
@@ -15,13 +26,18 @@ const AddPost = () => {
           "Content-type": "application/json; charset=UTF-8",
         },
       })
-        .then((response) => response.json())
+        .then((response) => {
+          setResponse(response);
+          setMessage(response.status);
+          setShowNotification(true);
+          response.json();
+        })
         .then((json) => console.log(json));
     } catch (error) {
       console.log("!error ", error);
     }
   };
-
+  console.log(errors);
   return (
     <>
       <MDBContainer className="mt-5 text-center mb-5 add-form-wrapper">
@@ -31,23 +47,56 @@ const AddPost = () => {
         <h2>Add New post</h2>
         <form onSubmit={handleSubmit(onSubmit)} className="d-flex flex-column">
           <input
+            name="title"
+            className={`${
+              errors && errors?.title?.type === "required" ? "required" : ""
+            }`}
             type="text"
             placeholder="Title"
             {...register("title", { required: true })}
           />
+          {errors && errors?.title?.type === "required" && (
+            <label className="required-label" htmlFor="title">
+              This field is required!
+            </label>
+          )}
           <input
+            name="body"
             type="text"
             placeholder="body"
+            className={`${
+              errors && errors?.body?.type === "required" ? "required" : ""
+            }`}
             {...register("body", { required: true })}
           />
+          {errors && errors?.body?.type === "required" && (
+            <label className="required-label" htmlFor="body">
+              This field is required!{" "}
+            </label>
+          )}
           <input
-            type="text"
+            name="userId"
+            type="number"
             placeholder="User id"
-            {...register("usedId", { required: true })}
+            className={`${
+              errors && errors?.userId?.type === "required" ? "required" : ""
+            }`}
+            {...register("userId", { required: true })}
           />
-
+          {console.log(errors)}
+          {errors && errors?.userId?.type === "required" && (
+            <label htmlFor="userId" className="required-label">
+              This field is required and must be number!
+            </label>
+          )}
           <input type="submit" className="submit-btn" />
         </form>
+        {showNotification && (
+          <Notification
+            title={`${response.ok ? "Success!" : "Error"}`}
+            message={`Server responded with status: ${message}`}
+          />
+        )}
       </MDBContainer>
     </>
   );
--- src/components/posts/edit/edit.jsx
+++ src/components/posts/edit/edit.jsx
@@ -1,26 +1,39 @@
 import { MDBContainer, MDBIcon } from "mdbreact";
 import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import Notification from "layout/notification";
 
 const EditPost = () => {
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm();
+  } = useForm({
+    mode: "onBlur",
+  });
+  const [response, setResponse] = useState("");
+  const [message, setMessage] = useState("");
+  const [showNotification, setShowNotification] = useState(false);
 
   const onSubmit = async (data) => {
     try {
-      fetch("https://jsonplaceholder.typicode.com/posts/1", {
+      fetch(`https://jsonplaceholder.typicode.com/posts/${data.id}`, {
         method: "PUT",
         body: JSON.stringify({ data }),
         headers: {
           "Content-type": "application/json; charset=UTF-8",
         },
       })
-        .then((response) => response.json())
+        .then((response) => {
+          setResponse(response);
+          setMessage(response.status);
+          setShowNotification(true);
+          response.json();
+        })
         .then((json) => console.log(json));
     } catch (error) {
+      console.log(error);
       console.log("!error", error);
     }
   };
@@ -37,25 +50,62 @@ const EditPost = () => {
             type="text"
             placeholder="Title"
             {...register("title", { required: true, maxLength: 80 })}
+            className={`${
+              errors && errors?.title?.type === "required" ? "required" : ""
+            }`}
           />
+          {errors && errors?.title?.type === "required" && (
+            <label className="required-label" htmlFor="title">
+              This field is required!
+            </label>
+          )}
           <input
             type="text"
             placeholder="Body"
+            className={`${
+              errors && errors?.body?.type === "required" ? "required" : ""
+            }`}
             {...register("body", { required: true, maxLength: 100 })}
           />
+          {errors && errors?.body?.type === "required" && (
+            <label className="required-label" htmlFor="body">
+              This field is required!{" "}
+            </label>
+          )}
           <input
-            type="text"
+            type="number"
             placeholder="User id"
+            className={`${
+              errors && errors?.userId?.type === "required" ? "required" : ""
+            }`}
             {...register("userId", { required: true, maxLength: 100 })}
           />
+          {errors && errors?.userId?.type === "required" && (
+            <label htmlFor="userId" className="required-label">
+              This field is required and must be number!
+            </label>
+          )}
           <input
-            type="text"
-            placeholder="ID"
+            type="number"
+            placeholder="Post id"
             {...register("id", { required: true, maxLength: 80 })}
+            className={`${
+              errors && errors?.id?.type === "required" ? "required" : ""
+            }`}
           />
-
+          {errors && errors?.id?.type === "required" && (
+            <label htmlFor="id" className="required-label">
+              This field is required and must be number!
+            </label>
+          )}
           <input type="submit" className="submit-btn" />
         </form>
+        {showNotification && (
+          <Notification
+            title={`${response.ok ? "Success!" : "Error"}`}
+            message={`Server responded with status: ${message}`}
+          />
+        )}
       </MDBContainer>
     </>
   );
